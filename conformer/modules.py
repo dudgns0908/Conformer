2a4b375c16a3540530ee2a516b28e8dd88548b19
Younghun <dudgns09081@gmail.com>
2021-02-16 02:24:50 +0900

    update modules.py and activations.py

--- conformer/modules.py
+++ conformer/modules.py
@@ -1,6 +1,6 @@
 import torch
 from torch import nn, Tensor
-from torch.nn.modules import activation
+from .activations import Swish
 
 
 class MultiHeadedSelfAttentionModule(nn.Module):
@@ -13,8 +13,32 @@ class MultiHeadedSelfAttentionModule(nn.Module):
 class ConvolutionModule(nn.Module):
     """ Convolution Module """
 
-    def __init__(self):
+    def __init__(
+            self,
+            in_channels: int,
+            expansion_factor: int = 2,
+            kernel_size: int = 32,
+            dropout_p: float = 0.1,
+            device: torch.device = 'cpu'
+    ):
         super().__init__()
+        self.device = device
+        inner_channels = in_channels * expansion_factor
+
+        self.sequential = nn.Sequential(
+            nn.LayerNorm(in_channels),
+            PointwiseConv1d(in_channels, inner_channels),
+            nn.GLU(dim=1),
+            DepthwiseConv1d(inner_channels, inner_channels, kernel_size, stride=1, padding=(kernel_size - 1) // 2),
+            nn.BatchNorm1d(inner_channels),
+            Swish(),
+            PointwiseConv1d(inner_channels, in_channels),
+            nn.Dropout(p=dropout_p)
+        )
+
+    def forward(self, inputs: Tensor):
+        inputs = inputs.to(self.device)
+        return self.sequential(inputs)
 
 
 class FeedForwardModule(nn.Module):
@@ -54,3 +78,45 @@ class ResidualModule(nn.Module):
     def forward(self, inputs: Tensor):
         module_output = self.module(inputs) * self.factor
         return module_output + inputs
+
+
+class PointwiseConv1d(nn.Module):
+    def __init__(
+            self,
+            in_channels: int,
+            out_channels: int,
+            stride: int = 1,
+            padding: int = 0,
+            dilation: int = 1,
+            bias: bool = True,
+    ):
+        super().__init__()
+        self.pointwise = nn.Conv1d(in_channels, out_channels, kernel_size=1, stride=stride, padding=padding,
+                                   dilation=dilation, groups=in_channels, bias=bias)
+
+    def forward(self, x):
+        out = self.pointwise(x)
+        return out
+
+
+class DepthwiseConv1d(nn.Module):
+    def __init__(
+            self,
+            in_channels: int,
+            out_channels: int,
+            kernel_size: int = 10,
+            stride: int = 1,
+            padding: int = 0,
+            dilation: int = 1,
+            bias: bool = True,
+    ):
+        super().__init__()
+        self.depthwise = nn.Conv1d(in_channels, out_channels, kernel_size, stride=stride, padding=padding,
+                                   dilation=dilation, groups=in_channels, bias=bias)
+
+    def forward(self, x):
+        return self.depthwise(x)
+
+
+if __name__ == '__main__':
+    ConvolutionModule()
