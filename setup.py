81896ab5b9dd4e94c7ef77593f1aa6cb64ae7836
Younghun Kim <dudgns09081@gmail.com>
2021-04-20 01:11:39 +0900

    install hydra

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Conformer',
+    version='1.0.0',
+    packages=['conformer'],
+    url='https://github.com/dudgns0908/Conformer',
+    license='Apache-2.0 License',
+    author='younghun',
+    author_email='dudgns09081@gmail.com',
+    description=''
+)
