faed26da1dff4421ec8eb6089d1eef5f27320866
Younghun Kim <dudgns09081@gmail.com>
2021-05-10 01:04:56 +0900

    remove MultiHeadAttentionWithRelativePositionalEmbedding

--- conformer/models/attention.py
+++ conformer/models/attention.py
@@ -24,19 +24,19 @@ class MultiHeadAttentionWithRelativePositionalEmbedding(nn.Module):
         self.num_heads = num_heads
         self.sqrt_dim = np.sqrt(dim)
 
-        self.positional_embedding = PositionalEmbedding(d_model=dim)
-        self.query_proj = nn.Linear(dim, dim)
-        self.key_proj = nn.Linear(dim, dim)
-        self.value_proj = nn.Linear(dim, dim)
-        self.pos_proj = nn.Linear(dim, dim, bias=False)
-
-        self.dropout = nn.Dropout(p=dropout_p)
-        self.u_bias = nn.Parameter(torch.Tensor(self.num_heads, self.d_head))
-        self.v_bias = nn.Parameter(torch.Tensor(self.num_heads, self.d_head))
-        torch.nn.init.xavier_uniform_(self.u_bias)
-        torch.nn.init.xavier_uniform_(self.v_bias)
-
-        self.out_proj = nn.Linear(dim, dim)
+        # self.positional_embedding = PositionalEmbedding(d_model=dim)
+        # self.query_proj = nn.Linear(dim, dim)
+        # self.key_proj = nn.Linear(dim, dim)
+        # self.value_proj = nn.Linear(dim, dim)
+        # self.pos_proj = nn.Linear(dim, dim, bias=False)
+        #
+        # self.dropout = nn.Dropout(p=dropout_p)
+        # self.u_bias = nn.Parameter(torch.Tensor(self.num_heads, self.d_head))
+        # self.v_bias = nn.Parameter(torch.Tensor(self.num_heads, self.d_head))
+        # torch.nn.init.xavier_uniform_(self.u_bias)
+        # torch.nn.init.xavier_uniform_(self.v_bias)
+        #
+        # self.out_proj = nn.Linear(dim, dim)
 
     def forward(
             self,
@@ -52,28 +52,28 @@ class MultiHeadAttentionWithRelativePositionalEmbedding(nn.Module):
         pos_embedding = self.positional_embedding(seq_length)
 
         # TODO:: need to understand...
-        query = self.query_proj(query).view(batch_size, -1, self.num_heads, self.d_head)
-        key = self.key_proj(key).view(batch_size, -1, self.num_heads, self.d_head).permute(0, 2, 1, 3)
-        value = self.value_proj(value).view(batch_size, -1, self.num_heads, self.d_head).permute(0, 2, 1, 3)
-        pos_embedding = self.pos_proj(pos_embedding).view(batch_size, -1, self.num_heads, self.d_head)
-
-        content_score = torch.matmul((query + self.u_bias).transpose(1, 2), key.transpose(2, 3))
-        pos_score = torch.matmul((query + self.v_bias).transpose(1, 2), pos_embedding.permute(0, 2, 3, 1))
-        pos_score = self._relative_shift(pos_score)
-
-        score = (content_score + pos_score) / self.sqrt_dim
-
-        if mask is not None:
-            mask = mask.unsqueeze(1)
-            score.masked_fill_(mask, -1e9)
-
-        attn = F.softmax(score, -1) 
-        attn = self.dropout(attn)
-
-        context = torch.matmul(attn, value).transpose(1, 2)
-        context = context.contiguous().view(batch_size, -1, self.dim)
-
-        return self.out_proj(context)
+        # query = self.query_proj(query).view(batch_size, -1, self.num_heads, self.d_head)
+        # key = self.key_proj(key).view(batch_size, -1, self.num_heads, self.d_head).permute(0, 2, 1, 3)
+        # value = self.value_proj(value).view(batch_size, -1, self.num_heads, self.d_head).permute(0, 2, 1, 3)
+        # pos_embedding = self.pos_proj(pos_embedding).view(batch_size, -1, self.num_heads, self.d_head)
+        #
+        # content_score = torch.matmul((query + self.u_bias).transpose(1, 2), key.transpose(2, 3))
+        # pos_score = torch.matmul((query + self.v_bias).transpose(1, 2), pos_embedding.permute(0, 2, 3, 1))
+        # pos_score = self._relative_shift(pos_score)
+        #
+        # score = (content_score + pos_score) / self.sqrt_dim
+        #
+        # if mask is not None:
+        #     mask = mask.unsqueeze(1)
+        #     score.masked_fill_(mask, -1e9)
+        #
+        # attn = F.softmax(score, -1)
+        # attn = self.dropout(attn)
+        #
+        # context = torch.matmul(attn, value).transpose(1, 2)
+        # context = context.contiguous().view(batch_size, -1, self.dim)
+        #
+        # return self.out_proj(context)
 
     def _relative_shift(self, pos_score: Tensor) -> Tensor:
         batch_size, num_heads, seq_length1, seq_length2 = pos_score.size()
