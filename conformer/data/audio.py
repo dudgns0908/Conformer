bc3fe38071608bdf916bbdd40028b76bd9952493
Younghun Kim <dudgns09081@gmail.com>
2021-06-23 03:34:22 +0900

    update AudioDataset

--- conformer/data/audio.py
+++ conformer/data/audio.py
@@ -4,18 +4,27 @@ import librosa
 import numpy as np
 
 
-def load_audio(path: str, sampling_rate: int = 16000):
+def load_audio(path: str, sampling_rate: int = 16000, del_silence: bool = False):
     try:
         extension = os.path.splitext(path)[-1][1:]
         if extension in ('wav',):
             signal, _ = librosa.load(path, sr=sampling_rate)
         elif extension == 'pcm':
             signal = np.memmap(path, dtype='h', mode='r').astype('float32')
-            return signal / 32767  # normalize audio
+
+            if del_silence:
+                non_silence_indices = librosa.effects.split(signal, top_db=30)
+                signal = np.concatenate([signal[start:end] for start, end in non_silence_indices])
+
+            signal /= 32767  # normalize audio
         else:
             raise ValueError(f'This extension ({extension}) is not supported')
 
+        return signal
+
     except FileNotFoundError as e:
+        raise e
+    except ValueError as e:
         raise e
 
 
--- conformer/data/data_loader.py
+++ conformer/data/data_loader.py
@@ -1,3 +1,6 @@
+import os
+from typing import Tuple, Any, Union
+
 from torch import Tensor
 from torch.utils.data import Dataset
 from .audio import load_audio
@@ -6,8 +9,9 @@ from .audio import load_audio
 class AudioDataset(Dataset):
     def __init__(
             self,
+            dataset_path: str,
             data_paths: list,
-            transcripts: list,
+            transcripts: list = None,
             sampling_rate: int = 16000,
             sos_id: int = 1,
             eos_id: int = 2,
@@ -15,11 +19,26 @@ class AudioDataset(Dataset):
     ) -> None:
         super().__init__()
         assert len(data_paths) == len(transcripts), 'audio_paths and transcripts must be the same length.'
-        self.data_paths = data_paths
 
-    def __getitem__(self, index) -> Tensor:
-        path, transcript = self.data_paths[index]
+        self.dataset_path = dataset_path
+        self.data_paths = data_paths
+        self.transcripts = transcripts
+
+    def __getitem__(self, index) -> Tuple[Tensor, Union[list, None]]:
+        data_path = os.path.join(self.dataset_path, self.data_paths[index])
+        data = self._get_audio_feature(data_path)
+
+        transcript = None
+        if self.transcripts is not None:
+            transcript = self.transcripts[index]
+
+        return data, transcript
+
+    def _get_audio_feature(self, path: str, ):
         signal = load_audio(path)
+        return signal
+
+
 
 
 class DataLoader:
