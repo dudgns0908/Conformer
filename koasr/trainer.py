c27399c36848eea993da007390ed90697f1795cb
Younghun Kim <dudgns09081@gmail.com>
2021-07-13 23:45:07 +0900

    update trainer.py

--- koasr/trainer.py
+++ koasr/trainer.py
@@ -14,15 +14,18 @@ class Trainer:
             dataset_dir: str,
             model_name: str,
             model_params: dict,
+            resume: bool = False,
     ):
         assert model_name.lower() in model_dict.keys(), f'This is Not supported model name ({model_name})'
 
         self.dataset_dir = dataset_dir
         # self.transcript_path = transcript_path
 
-
+        # Read data and preprocessing
         self.datasets = AudioDataset(dataset_dir, audio_paths=[], transcripts=[])
         # self.model = model_dict[model_name](vocab_size=80, **model_params)
+
+        # Load model
         self.model = Conformer(**model_params)
 
     def fit(self):
